946394f68e381c87638b20990bc502407b64de26
ThibautRingdev <thibautringeval@gmail.com>
2026-01-03 13:53:15 +0100

    add counter with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Counter from './components/Counter'
 
 function App() {
   return (
-    <div>App</div>
+    <div><Counter /></div>
   )
 }
 
---
+++ src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Counter() {
+    const [count, setCount] = useState(0)
+  return (
+    <div className='container'>
+        <div>
+            <h1 className='number'>{count}</h1>
+        </div>
+        <div>
+            <button className='btn' onClick={() => setCount(count + 1)}>Increment</button>
+            <button className='btn' onClick={() => setCount(count - 1)}>Decrement</button>
+            <button className='btn' onClick={() => setCount(0)}>Reset</button>
+        </div>
+    </div>
+  )
+}
+
+export default Counter
